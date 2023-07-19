2dcc3e1e4dad841a876721efbf711f3798dc85d8
Kris Braun <kris.braun@letscollide.io>
2023-07-18 20:22:27 -0400

    Test exceptions

--- apps/web/app/routes/_public._index.tsx
+++ apps/web/app/routes/_public._index.tsx
@@ -21,6 +21,13 @@ export default function Index() {
         </Button>
       )}
       {user && <Button onClick={logout}>Sign out</Button>}
+      <Button
+        onClick={() => {
+          throw Error("Boom!");
+        }}
+      >
+        Blow up
+      </Button>
       <ul>
         <li>
           <a href="https://remix.run/">Remix</a>
